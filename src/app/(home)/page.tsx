4c6872703756c3510caa1749a7cabf5dbfe8c006
Lucas Cristovam <cristhovamlucas@gmail.com>
2023-10-20 14:05:36 -0300

    feat: adding new section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import Categories from './components/categories';
 import { db } from '@/lib/prisma';
 import ProductList from './components/product-list';
+import SectionTitle from './components/section-title';
+import PromoBanner from './components/promo-banner';
 
 export default async function Home() {
 	const deals = await db.product.findMany({
@@ -12,31 +14,35 @@ export default async function Home() {
 		}
 	});
 
+	const keyboards = await db.product.findMany({
+		where: {
+			category: {
+				slug: 'keyboards'
+			}
+		}
+	});
+
 	return (
 		<div>
-			<Image
+			<PromoBanner
 				src='/banner-home-01.png'
 				alt='Até 55% de desconto só esse mês!'
-				height={150}
-				width={350}
-				className='h-auto w-full px-5'
-				sizes='100vw'
 			/>
 			<div className='mt-8 px-5'>
 				<Categories />
 			</div>
 			<div className='mt-8'>
-				<p className='pl-5 font-semibold uppercase mb-5'>Ofertas</p>
+				<SectionTitle>Ofertas</SectionTitle>
 				<ProductList products={deals} />
 			</div>
-			<Image
+			<PromoBanner
 				src='/banner-home-02.png'
 				alt='Até 55% de desconto em mouses!'
-				height={150}
-				width={350}
-				className='h-auto w-full px-5 mt-8'
-				sizes='100vw'
 			/>
+			<div className='mt-8'>
+				<SectionTitle>Teclados</SectionTitle>
+				<ProductList products={keyboards} />
+			</div>
 		</div>
 	);
 };
