6f8e26db287f5f11c06f31d21965bcd93122ac02
Lucas Cristovam <cristhovamlucas@gmail.com>
2023-10-20 13:51:02 -0300

    feat: adding product list and second banner

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,22 +1,42 @@
-'use client';
-
 import Image from 'next/image';
 import Categories from './components/categories';
+import { db } from '@/lib/prisma';
+import ProductList from './components/product-list';
 
-export default function Home() {
+export default async function Home() {
+	const deals = await db.product.findMany({
+		where: {
+			discountPercentage: {
+				gt: 0
+			}
+		}
+	});
+
 	return (
-		<div className='p-5'>
+		<div>
 			<Image
 				src='/banner-home-01.png'
 				alt='Até 55% de desconto só esse mês!'
 				height={150}
 				width={350}
-				className='h-auto w-full'
+				className='h-auto w-full px-5'
 				sizes='100vw'
 			/>
-			<div className='mt-8'>
+			<div className='mt-8 px-5'>
 				<Categories />
 			</div>
+			<div className='mt-8'>
+				<p className='pl-5 font-semibold uppercase mb-5'>Ofertas</p>
+				<ProductList products={deals} />
+			</div>
+			<Image
+				src='/banner-home-02.png'
+				alt='Até 55% de desconto em mouses!'
+				height={150}
+				width={350}
+				className='h-auto w-full px-5 mt-8'
+				sizes='100vw'
+			/>
 		</div>
 	);
 };
---
+++ src/components/ui/product-item.tsx
@@ -0,0 +1,45 @@
+import { ProductWithTotalPrice } from '@/helpers/product';
+import Image from 'next/image';
+import { Badge } from './badge';
+import { ArrowDownIcon } from 'lucide-react';
+
+interface ProductItemProps {
+	product: ProductWithTotalPrice;
+};
+
+const ProductItem = ({ product }: ProductItemProps) => {
+	return (
+		<div className='flex max-w-[156px] flex-col gap-4'>
+			<div className='relative bg-accent rounded-lg h-[170px] w-[156px] flex items-center justify-center'>
+				<Image src={product.imageUrls[0]} height={0} width={0} sizes='100vw' className='h-auto max-h-[70%] w-auto max-w-[80%]' style={{ objectFit: 'contain' }} alt={product.name} />
+				{
+					product.discountPercentage > 0 && (
+						<Badge className='absolute left-2 top-2 px-2 py-[2px]'>
+							<ArrowDownIcon size={14} />
+							{product.discountPercentage}%
+						</Badge>
+					)
+				}
+			</div>
+			<div className='flex flex-col gap-1'>
+				<div>
+					<p className='text-sm overflow-hidden whitespace-nowrap text-ellipsis'>{product.name}</p>
+				</div>
+				<div className='flex items-center gap-2'>
+					{
+						product.discountPercentage > 0 ? (
+							<>
+								<p className='font-semibold text-base'>R$ {product.totalPrice.toFixed(2)}</p>
+								<p className='opacity-75 line-through text-xs'>R$ {Number(product.basePrice).toFixed(2)}</p>
+							</>
+						) : (
+							<p className='font-semibold text-sm'>R$ {product.basePrice.toFixed(2)}</p>
+						)
+					}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default ProductItem;
