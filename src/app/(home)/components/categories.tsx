c4125446c5c804331fe74f6deae73504fbca0a5c
Lucas Cristovam <cristhovamlucas@gmail.com>
2023-10-20 09:20:44 -0300

    feat: adding Categories and CategoryItem components

---
+++ src/app/(home)/components/categories.tsx
@@ -0,0 +1,18 @@
+import { db } from "@/lib/prisma";
+import CategoryItem from "./category-item";
+
+const Categories = async () => {
+	const categories = await db.category.findMany({});
+
+	return (
+		<div className='grid grid-cols-2 gap-x-4 gap-y-2'>
+			{
+				categories.map((category) => (
+					<CategoryItem category={category} key={category.id} />
+				))
+			}
+		</div>
+	);
+}
+
+export default Categories;
