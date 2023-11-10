644a18107ed81a37e4bf30167cef639b3fffd611
Lucas Cristovam <cristhovamlucas@gmail.com>
2023-11-10 14:51:24 -0300

    feat: removing products from cart

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -13,6 +13,7 @@ interface ICartContext {
 	cartBasePrice: number;
 	cartTotalDiscount: number;
 	addProductToCart: (product: CartProduct) => void;
+	decreaseProductQuantity: (productId: string) => void;
 };
 
 export const CartContext = createContext<ICartContext>({
@@ -20,7 +21,8 @@ export const CartContext = createContext<ICartContext>({
 	cartBasePrice: 0,
 	cartTotalDiscount: 0,
 	cartTotalPrice: 0,
-	addProductToCart: () => {}
+	addProductToCart: () => { },
+	decreaseProductQuantity: () => { },
 });
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -45,13 +47,27 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
 		setProducts((prev) => [...prev, product]);
 	};
 
+	const decreaseProductQuantity = (productId: string) => {
+		setProducts((prev) => prev.map((cartProduct) => {
+			if (cartProduct.id === productId) {
+				return {
+					...cartProduct,
+					quantity: cartProduct.quantity - 1
+				};
+			};
+
+			return cartProduct;
+		}).filter((cartProduct) => cartProduct.quantity > 0));
+	};
+
 	return (
 		<CartContext.Provider value={{
 			products: [],
 			cartTotalPrice: 0,
 			cartBasePrice: 0,
 			cartTotalDiscount: 0,
-			addProductToCart
+			addProductToCart,
+			decreaseProductQuantity
 		}}>
 			{children}
 		</CartContext.Provider>
